2f269c3fb1f35bae634f730ba6cf4bf50ee7ff8e
OnkarKulkarni2001 <98707888+OnkarKulkarni2001@users.noreply.github.com>
2024-10-27 03:19:06 -0400

    Delete MediaPlayer&ChannelGroupManager_With_GUI/.vshistory directory

--- MediaPlayer&ChannelGroupManager_With_GUI/.vshistory/AudioUtils.hpp/2024-09-20_13_43_30_931.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <fmod.h>
-
-namespace audio
-{
-	void CheckError(FMOD_RESULT result, const char* file, int line);
-#define FMODCheckError(result) CheckError(result,__FILE__, __LINE__);
-}
