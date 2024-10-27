25b2914d6379d8a94121236625bb2e6f4f1e5e7c
OnkarKulkarni2001 <onkarkulkarni492001@gmail.com>
2024-10-27 16:50:43 -0400

    safafsafafa

---
+++ MediaPlayer_Scene_GUI/cAudioManager.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "fmod.hpp"
+#include <vector>
+
+class cAudioManager {
+public:
+	FMOD::Channel* channel; 
+	std::vector<FMOD::Sound*> sounds; // Vector to hold multiple sound pointers
+
+	bool bIsPlaying = false;
+	bool bIsPaused = false;
+	float pitchValue = 0;
+	float panValue = 0;
+	float volumeValue = 0;
+
+	// Load and unload function definitions
+	void loadAssets();
+	void unloadAssets();
+
+	// Sound control function definitions
+	void play(int index);
+	void pause();
+	void resume();
+	void stop();
+	void setPitch(float pitchValue);
+	void setVolume(float volumeValue);
+	void setPan(float panValue);
+};
